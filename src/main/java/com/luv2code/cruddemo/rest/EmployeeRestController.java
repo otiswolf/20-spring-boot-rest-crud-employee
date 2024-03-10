86ef1ec12246195479077e947ca16d13c543f44c
Otis Wolf <wolfotis@gmail.com>
2024-03-10 00:10:45 -0600

    add EmployeeRestController

---
+++ src/main/java/com/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.luv2code.cruddemo.rest;
+
+import com.luv2code.cruddemo.dao.EmployeeDao;
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDao employeeDao;
+
+    public EmployeeRestController(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDao.findAll();
+    }
+}
